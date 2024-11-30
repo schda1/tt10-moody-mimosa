2c5a6b25abce9166babd9e5008141064aaf26a2a
David Schmidiger <schmdavi@gmail.com>
2024-11-30 13:19:46 +0000

    Updated project structure
    Added scripts for hardening
    and rtl tests

---
+++ scripts/harden.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# The tt support tools are already installed in the docker container. The
+# procedure is thus slightly different than described in the tiny tapeout
+# local-hardening guide.
+
+if [ ! -d "scripts" ]; then
+  cd ..
+fi
+
+# Fetch the tt support tools if needed
+if [ ! -d "tt" ]; then
+    git clone -b tt10 https://github.com/TinyTapeout/tt-support-tools tt
+else
+    echo "tt support tools already cloned"
+fi
+
+# Harden design with openlane2
+python /ttsetup/tt-support-tools/tt_tool.py --create-user-config --openlane2
+python /ttsetup/tt-support-tools/tt_tool.py --harden --openlane2
+python /ttsetup/tt-support-tools/tt_tool.py --print-warnings --openlane2
+
+# Create overview of gds rendered layout as png
+rm -f gds_renderer.png gds_renderer_preview.svg
+python /ttsetup/tt-support-tools/tt_tool.py --create-png --openlane2
+
+# Copy gate level netlist to test folder
+TOP_MODULE=$(python /ttsetup/tt-support-tools/tt_tool.py --print-top-module)
+cp runs/wokwi/final/pnl/$TOP_MODULE.pnl.v test/gate_level_netlist.v
--- test/test.py
+++ test/test.py
@@ -20,7 +20,7 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 4)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
@@ -30,11 +30,11 @@ async def test_project(dut):
     dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 40)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
