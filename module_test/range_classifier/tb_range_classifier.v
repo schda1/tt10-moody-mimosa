a9259d3979a94fd1c4ce0eaf39e024cd2fd99d2b
David Schmidiger <schmdavi@gmail.com>
2024-12-02 21:19:35 +0000

    Made range classifier more flexible
    Input number range (bit length)
    can be set as a module parameter.

--- module_test/range_classifier/tb_range_classifier.v
+++ module_test/range_classifier/tb_range_classifier.v
@@ -11,14 +11,21 @@ module tb_range_classifier ();
   end
 
   // Wire up the inputs and outputs:
-  reg [6:0] number;
-  wire [1:0] out_bits;
+  reg [6:0] number_7bit;
+  reg [9:0] number_10bit;
+  wire [1:0] out_bits_7bit;
+  wire [1:0] out_bits_10bit;
   reg clk;
 
   // Instantiate range classifier module 
-  range_classifier dut (
-      .number  (number),   
-      .out_bits (out_bits)
+  range_classifier #(.N(7)) dut_range_classifier_7bit (
+      .number  (number_7bit),   
+      .out_bits (out_bits_7bit)
+  );
+
+  range_classifier #(.N(10)) dut_range_classifier_10bit (
+      .number  (number_10bit),   
+      .out_bits (out_bits_10bit)
   );
 
 endmodule
--- src/range_classifier.v
+++ src/range_classifier.v
@@ -1,16 +1,12 @@
+`default_nettype none
 
-module range_classifier (
-    input  [6:0] number,   // 7-bit input number
-    output [1:0] out_bits  // 2-bit output representing the range
+module range_classifier #(parameter N = 7) (
+    input  [N-1:0] number,   // N-bit input number
+    output [1:0] out_bits    // 2-bit output representing the range classifier
 );
 
-    // The output 'out_bits' is encoded as follows:
-    // 00: number <= 31
-    // 01: 32 <= number <= 95
-    // 10: number >= 96
-
-    assign out_bits = (number[6:5] == 2'b11) ? 2'b10 :  // Range 96 to 127
-                      (number[6:5] == 2'b00) ? 2'b00 :  // Range 0 to 31
-                                               2'b01;   // Range 32 to 95
+    assign out_bits = (number[N-1:N-2] == 2'b11) ? 2'b10 :   // Upper 1/4
+                      (number[N-1:N-2] == 2'b00) ? 2'b00 :   // Lower 1/4
+                                                   2'b01 ;   // Middle 1/2
 
 endmodule
