3aa4d155f1984c7ff114df73d8a92211e235e0b0
David Schmidiger <schmdavi@gmail.com>
2024-12-02 20:52:17 +0000

    Added dynamic clock divider and test
    Allows to dynamically adjust the clock
    division based on an input variable.
    
    The input clock can be directly fed
    through (x=0), reduced to half (x=1),
    a thrid (x=2) etc.
    
    The max. clock division (i.e. the number
    of bits for the variable x) can be set as
    a module parameter.

---
+++ module_test/dynamic_clock_divider/test_dynamic_clock_divider.py
@@ -0,0 +1,104 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles, Timer, RisingEdge, FallingEdge, Edge
+
+async def init(dut):
+    dut._log.info("Start")
+
+    # Set the clock period to 100ns (10 MHz)
+    clock = Clock(dut.clk, 100, units="ns")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut.x_2bit.value = 0
+    dut.x_4bit.value = 0
+    dut.clk_out_2bit.value = 0
+    dut.clk_out_4bit.value = 0
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 1)
+
+@cocotb.test()
+async def test_sat_counter_reset(dut):
+    """
+    Check whether reset sets value to correct value
+    """
+    await init(dut)
+
+    dut.x_4bit.value = 8
+    await ClockCycles(dut.clk, 3)
+    assert dut.clk_out_4bit.value != 0
+
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 2)
+    assert dut.clk_out_4bit.value == 0
+
+@cocotb.test()
+async def test_no_divider(dut):
+    """
+    Check whether output clock equals input clock for x = 0
+    """
+    await init(dut)
+
+    dut.x_4bit.value = 0
+
+    for i in range(10):
+        await RisingEdge(dut.clk)
+        await Timer(1, units='ns')
+        assert dut.clk_out_4bit.value == dut.clk.value
+
+        await FallingEdge(dut.clk)
+        await Timer(1, units='ns')
+        assert dut.clk_out_4bit.value == dut.clk.value
+    
+@cocotb.test()
+async def test_divide_by_2(dut):
+    """
+    Check whether output clock is half for x = 1
+    """
+    await init(dut)
+
+    dut.x_4bit.value = 1
+    await FallingEdge(dut.clk)
+
+    for i in range(10):
+
+        for low in range(2):
+            await Edge(dut.clk)
+            await Timer(1, units='ns')
+            assert dut.clk_out_4bit.value == 0
+        
+        for high in range(2):
+            await Edge(dut.clk)
+            await Timer(1, units='ns')
+            assert dut.clk_out_4bit.value == 1
+
+@cocotb.test()
+async def test_divide_by_4(dut):
+    """
+    Check whether output clock is a quarter for x = 3
+    """
+    await init(dut)
+
+    dut.x_4bit.value = 3
+    await ClockCycles(dut.clk, 1)
+    await Edge(dut.clk)
+
+    for i in range(10):
+
+        # Four edges LOW
+        for low in range(4):
+            await Edge(dut.clk)
+            await Timer(1, units='ns')
+            assert dut.clk_out_4bit.value == 0
+        
+        # Four edges HIGH
+        for high in range(4):
+            await Edge(dut.clk)
+            await Timer(1, units='ns')
+            assert dut.clk_out_4bit.value == 1
