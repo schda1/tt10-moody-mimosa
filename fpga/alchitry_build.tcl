a0c5c2501b98f053556d8ed82b14075e6f9a7521
David Schmidiger <schmdavi@gmail.com>
2024-12-06 22:00:36 +0000

    Added moody mimosa build for fpga
    Moody mimosa module can now be
    instantiated and the FPGA inputs
    and outputs are correctly routed to
    the button and switch inputs and
    led outputs of the alchitry Io board.

--- fpga/alchitry_build.tcl
+++ fpga/alchitry_build.tcl
@@ -2,11 +2,12 @@
 create_project -force test_project ./build -part xc7a35tftg256-1 
 
 # Add source files
-add_files {src/au_top.v src/reset_conditioner.v}
+add_files -norecurse ../src
+add_files -norecurse src
 add_files -fileset constrs_1 src/au.xdc
 
 # Set the top module and settings
-set_property top au_top [current_fileset]
+set_property top au_moody_mimosa [current_fileset]
 set_property STEPS.WRITE_BITSTREAM.ARGS.BIN_FILE true [get_runs impl_1]
 
 # Build bitstream
@@ -14,4 +15,4 @@ launch_runs impl_1 -to_step write_bitstream -jobs 8
 wait_on_runs impl_1
 
 # Copy the bin file from the build folder 
-file copy -force build/test_project.runs/impl_1/au_top.bin .
+file copy -force build/test_project.runs/impl_1/au_moody_mimosa.bin .
