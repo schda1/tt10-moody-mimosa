3e4857fb9f9a07e09babbb02fb802427d419a570
David Schmidiger <schmdavi@gmail.com>
2024-12-07 09:31:35 +0000

    Adapted range classification to 2-bit
    Range classifications uses upper 2 bits
    to classify the ressource value.

--- src/range_classifier.v
+++ src/range_classifier.v
@@ -1,16 +1,10 @@
+`default_nettype none
 
-module range_classifier (
-    input  [6:0] number,   // 7-bit input number
-    output [1:0] out_bits  // 2-bit output representing the range
+module range_classifier #(parameter N = 7) (
+    input  wire [N-1:0] number,   // N-bit input number
+    output wire [1:0] out_bits    // 2-bit output representing the range classifier
 );
 
-    // The output 'out_bits' is encoded as follows:
-    // 00: number <= 31
-    // 01: 32 <= number <= 95
-    // 10: number >= 96
-
-    assign out_bits = (number[6:5] == 2'b11) ? 2'b10 :  // Range 96 to 127
-                      (number[6:5] == 2'b00) ? 2'b00 :  // Range 0 to 31
-                                               2'b01;   // Range 32 to 95
+    assign out_bits = number[N-1:N-2];
 
 endmodule
