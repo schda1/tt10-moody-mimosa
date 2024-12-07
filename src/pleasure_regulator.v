450cda35225228ee10c36e5f7469a2fb56448ecb
David Schmidiger <schmdavi@gmail.com>
2024-12-07 11:48:31 +0000

    Added minimal pleasure regulator and test

---
+++ src/pleasure_regulator.v
@@ -0,0 +1,15 @@
+`default_nettype none
+
+module pleasure_regulator (
+    input wire sleep_controller_inc,
+    input wire sleep_controller_dec,
+    input wire [6:0] stimuli,
+    output wire pleasure_inc, 
+    output wire pleasure_dec
+);
+
+    // Currently no complex decision
+    assign pleasure_inc = stimuli[1] ? 1'b1 : sleep_controller_inc;
+    assign pleasure_dec = stimuli[1] ? 1'b0 : sleep_controller_dec;
+
+endmodule
