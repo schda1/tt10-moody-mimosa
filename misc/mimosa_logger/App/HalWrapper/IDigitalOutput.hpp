adfa2b50311467d354099966d4a576eaad8dc3e6
David Schmidiger <schmdavi@gmail.com>
2025-01-02 19:33:44 +0000

    Added wrapper for GPIO Inputs and Outputs
    Basic wrapper classes for digital inputs and
    digital outputs, derived from the (nearly)
    abstract IDigitalInput and IDigitalOutput
    classes. They allow to easily set up mocks
    for unit testing.

---
+++ misc/mimosa_logger/App/HalWrapper/IDigitalOutput.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+class IDigitalOutput
+{
+public:
+    IDigitalOutput() {}
+    virtual ~IDigitalOutput() {}
+
+    virtual void init() = 0;
+
+    virtual void set(uint8_t state) = 0;
+    virtual uint8_t get() const = 0;
+    virtual void toggle() = 0;
+    virtual void invert(bool invert) = 0;
+};
