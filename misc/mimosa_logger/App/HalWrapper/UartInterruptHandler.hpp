b7a0cfbe1cee736f064b70c731653e15b338bf58
David Schmidiger <schmdavi@gmail.com>
2025-01-02 19:35:30 +0000

    Added minimalistic uart wrapper
    Minimal wrapper classes for uart. They
    are minimal, this whole project is a personal
    exerpiment.

---
+++ misc/mimosa_logger/App/HalWrapper/UartInterruptHandler.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <HalWrapper/IUart.hpp>
+#include "stm32g4xx_hal.h"
+
+#define N_UART (6)
+
+class UartInterruptHandler
+{
+public:
+    static UartInterruptHandler* getInstance()
+    {
+        if (handler == nullptr) {
+            handler = new UartInterruptHandler();
+        }
+        return handler;
+    }
+
+    void attach(IUart* uart);
+    void detach(IUart* uart);
+
+    IUart* get_uart(USART_TypeDef* instance);
+
+private:
+    UartInterruptHandler();
+    UartInterruptHandler(UartInterruptHandler const&);
+    void operator=(UartInterruptHandler const&);
+
+    int8_t get_uart_id(USART_TypeDef* instance);
+
+    static UartInterruptHandler* handler;
+    IUart* uarts[N_UART];
+};
