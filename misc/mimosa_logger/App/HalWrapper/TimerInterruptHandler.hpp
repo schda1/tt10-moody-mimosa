5f00acf7cf5e6cf16f4ff0a2b98cdce5625f31ad
David Schmidiger <schmdavi@gmail.com>
2025-01-02 19:34:40 +0000

    Added minimalistic timer wrappers
    Minimal wrapper classes for timers. They
    are minimal, this whole project is a personal
    exerpiment.

---
+++ misc/mimosa_logger/App/HalWrapper/TimerInterruptHandler.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <HalWrapper/ITimer.hpp>
+#include "stm32g4xx_hal.h"
+
+#define N_TIMER (12)
+
+class TimerInterruptHandler
+{
+public:
+    static TimerInterruptHandler* getInstance()
+    {
+        if (handler == nullptr) {
+            handler = new TimerInterruptHandler();
+        }
+        return handler;
+    }
+
+    void attach(ITimer* timer);
+    void detach(ITimer* timer);
+
+    ITimer* get_timer(TIM_TypeDef* instance);
+
+private:
+    TimerInterruptHandler();
+    TimerInterruptHandler(TimerInterruptHandler const&);
+    void operator=(TimerInterruptHandler const&);
+
+    int8_t get_timer_id(TIM_TypeDef* instance);
+
+    static TimerInterruptHandler* handler;
+    ITimer* timers[N_TIMER];
+};
