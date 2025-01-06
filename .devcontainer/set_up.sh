d8e9549402476992617ac7e663584b7acc1bb780
David Schmidiger <schmdavi@gmail.com>
2025-01-06 12:46:43 +0000

    Removed logger application

--- .devcontainer/set_up.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-pip install -r /workspaces/tt10-moody-mimosa/test/requirements.txt
-pip install -r /workspaces/tt10-moody-mimosa/simulation/requirements.txt
-git submodule init
-git submodule update --init --recursive
--- scripts/set_up_dependencies.sh
+++ scripts/set_up_dependencies.sh
@@ -6,31 +6,7 @@ if [ ! -d "scripts" ] || [ ! -d "test" ]; then
 fi
 
 PRJ=$(pwd)
-CPPUTEST=$PRJ/misc/mimosa_logger/cpputest
 
 # Install python dependencies
 pip install -r /workspaces/tt10-moody-mimosa/test/requirements.txt
 pip install -r /workspaces/tt10-moody-mimosa/simulation/requirements.txt
-
-# Initialize git submodules (ST Drivers, cpputest)
-git submodule init
-git submodule update --init --recursive
-
-# Slight modification of the cpputest configure script
-source scripts/add_ranlib_to_cpputest_configure.sh
-
-# Build cpputest
-cd misc/mimosa_logger/cpputest
-mkdir cpputest_build
-cd cpputest_build
-autoreconf .. -i
-../configure
-make
-
-# Copy built libraries
-mkdir -p $CPPUTEST/lib
-cp -f $CPPUTEST/cpputest_build/lib/libCppUTest.a $CPPUTEST/lib/libCppUTest.a
-cp -f $CPPUTEST/cpputest_build/lib/libCppUTestExt.a $CPPUTEST/lib/libCppUTestExt.a
-
-# Return to the project root folder
-cd $PRJ
